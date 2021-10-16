e6ec26eebc8f5d1475e1dd073c2e31102b5903f3
kash1064 <kaship24@gmail.com>
2021-10-17 00:18:51 +0900

    add switch program

---
+++ src/c/switch_task.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        printf("Invalid arguments.\n");
+    }
+    else {
+        printf("Start!!\n");
+    }
+    return 0;
+}
