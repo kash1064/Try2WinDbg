bee20225a8108cd61981097473a903b2c2a34d0f
kash1064 <kaship24@gmail.com>
2021-12-17 22:52:05 +0900

    FirstDriverSample created

---
+++ drivers/FirstDriverSample/FirstDriver.cpp
@@ -0,0 +1,16 @@
+#include <ntddk.h>
+
+void FirstDriverUnload(_In_ PDRIVER_OBJECT DriverObject)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+}
+
+extern "C"
+NTSTATUS
+DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	return STATUS_SUCCESS;
+}
