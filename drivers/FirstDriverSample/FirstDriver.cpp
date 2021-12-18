c08f0b2a14d57eec61da9beba52cc71292987c18
kash1064 <kaship24@gmail.com>
2021-12-18 11:56:47 +0900

    add unload ruitine

--- drivers/FirstDriverSample/FirstDriver.cpp
+++ drivers/FirstDriverSample/FirstDriver.cpp
@@ -9,8 +9,9 @@ extern "C"
 NTSTATUS
 DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 {
-	UNREFERENCED_PARAMETER(DriverObject);
 	UNREFERENCED_PARAMETER(RegistryPath);
+
+	DriverObject->DriverUnload = FirstDriverUnload;
 
 	return STATUS_SUCCESS;
 }
