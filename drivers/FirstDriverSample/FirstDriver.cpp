109034beac5f98c4224c937dbb8cd730b1abc269
kash1064 <kaship24@gmail.com>
2021-12-19 23:54:16 +0900

    add refer os versions to sample driver

--- drivers/FirstDriverSample/FirstDriver.cpp
+++ drivers/FirstDriverSample/FirstDriver.cpp
@@ -3,6 +3,8 @@
 void FirstDriverUnload(_In_ PDRIVER_OBJECT DriverObject)
 {
 	UNREFERENCED_PARAMETER(DriverObject);
+
+	KdPrint(("This driver unloaded\n"));
 }
 
 extern "C"
@@ -12,6 +14,14 @@ DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 	UNREFERENCED_PARAMETER(RegistryPath);
 
 	DriverObject->DriverUnload = FirstDriverUnload;
+
+	OSVERSIONINFOEXW osVersionInfo;
+	NTSTATUS status = STATUS_SUCCESS;
+	osVersionInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
+	status = RtlGetVersion((POSVERSIONINFOW)&osVersionInfo);
+
+	KdPrint(("This is my first sample driver\n"));
+	KdPrint(("OS version is : %d.%d.%d\n", osVersionInfo.dwMajorVersion, osVersionInfo.dwMinorVersion, osVersionInfo.dwBuildNumber));
 
 	return STATUS_SUCCESS;
 }
