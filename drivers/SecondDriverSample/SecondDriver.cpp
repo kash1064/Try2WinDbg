55f514e14d685bb3ecf56b4ea80835a295651bf0
kash1064 <kaship24@gmail.com>
2022-01-26 18:58:15 +0900

    add SecondDriverSample solution

---
+++ drivers/SecondDriverSample/SecondDriver.cpp
@@ -0,0 +1,27 @@
+#include <ntddk.h>
+
+void SecondDriverUnload(_In_ PDRIVER_OBJECT DriverObject)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+
+	KdPrint(("This driver unloaded\n"));
+}
+
+extern "C"
+NTSTATUS
+DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
+{
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	DriverObject->DriverUnload = SecondDriverUnload;
+
+	OSVERSIONINFOEXW osVersionInfo;
+	NTSTATUS status = STATUS_SUCCESS;
+	osVersionInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
+	status = RtlGetVersion((POSVERSIONINFOW)&osVersionInfo);
+
+	KdPrint(("This is my first sample driver\n"));
+	KdPrint(("OS version is : %d.%d.%d\n", osVersionInfo.dwMajorVersion, osVersionInfo.dwMinorVersion, osVersionInfo.dwBuildNumber));
+
+	return STATUS_SUCCESS;
+}
