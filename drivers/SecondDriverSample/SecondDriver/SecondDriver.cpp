ab0bce1cb59e2e8f96057b55d852fb52237669eb
kash1064 <kaship24@gmail.com>
2022-01-29 14:11:26 +0900

    fix build

--- drivers/SecondDriverSample/SecondDriver/SecondDriver.cpp
+++ drivers/SecondDriverSample/SecondDriver/SecondDriver.cpp
@@ -3,51 +3,6 @@
 #include "SecondDriverCommon.h"
 
 void SecondDriverUnload(_In_ PDRIVER_OBJECT DriverObject);
-NTSTATUS SecondDriverCreateClose(_In_ PDRIVER_OBJECT DriverObject, _In_ PIRP irp);
-NTSTATUS SecondDriverDeviceControl(_In_ PDRIVER_OBJECT DriverObject, _In_ PIRP irp);
-
-
-void SecondDriverUnload(_In_ PDRIVER_OBJECT DriverObject)
-{
-	UNREFERENCED_PARAMETER(DriverObject);
-
-	KdPrint(("This driver unloaded\n"));
-}
-
-extern "C" NTSTATUS
-DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
-	UNREFERENCED_PARAMETER(RegistryPath);
-
-	KdPrint(("SecondDriver DriverEntry started\n"));
-
-	DriverObject->DriverUnload = SecondDriverUnload;
-
-	DriverObject->MajorFunction[IRP_MJ_CREATE] = SecondDriverCreateClose;
-	DriverObject->MajorFunction[IRP_MJ_CLOSE] = SecondDriverCreateClose;
-	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = SecondDriverDeviceControl;
-
-	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\SecondDriver");
-
-	//RtlInitUnicodeString(&devName, L"\\Device\\ThreadBoost");
-	PDEVICE_OBJECT DeviceObject;
-	NTSTATUS status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
-	if (!NT_SUCCESS(status)) {
-		KdPrint(("Failed to create device (0x%08X)\n", status));
-		return status;
-	}
-
-	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\SecondDriver");
-	status = IoCreateSymbolicLink(&symLink, &devName);
-	if (!NT_SUCCESS(status)) {
-		KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
-		IoDeleteDevice(DeviceObject);
-		return status;
-	}
-
-	KdPrint(("SecondDriverSecondDriver DriverEntry completed successfully\n"));
-
-	return STATUS_SUCCESS;
-}
 
 _Use_decl_annotations_
 NTSTATUS SecondDriverCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
@@ -106,4 +61,46 @@ NTSTATUS SecondDriverDeviceControl(PDEVICE_OBJECT, PIRP Irp) {
 	Irp->IoStatus.Information = 0;
 	IoCompleteRequest(Irp, IO_NO_INCREMENT);
 	return status;
+}
+
+void SecondDriverUnload(_In_ PDRIVER_OBJECT DriverObject)
+{
+	UNREFERENCED_PARAMETER(DriverObject);
+
+	KdPrint(("This driver unloaded\n"));
+}
+
+extern "C" NTSTATUS
+DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	KdPrint(("SecondDriver DriverEntry started\n"));
+
+	DriverObject->DriverUnload = SecondDriverUnload;
+
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = SecondDriverCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = SecondDriverCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = SecondDriverDeviceControl;
+
+	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\SecondDriver");
+
+	//RtlInitUnicodeString(&devName, L"\\Device\\ThreadBoost");
+	PDEVICE_OBJECT DeviceObject;
+	NTSTATUS status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create device (0x%08X)\n", status));
+		return status;
+	}
+
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\SecondDriver");
+	status = IoCreateSymbolicLink(&symLink, &devName);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
+		IoDeleteDevice(DeviceObject);
+		return status;
+	}
+
+	KdPrint(("SecondDriverSecondDriver DriverEntry completed successfully\n"));
+
+	return STATUS_SUCCESS;
 }
--- drivers/SecondDriverSample/SecondDriverClient/SecondDriverClient.cpp
+++ drivers/SecondDriverSample/SecondDriverClient/SecondDriverClient.cpp
@@ -1,20 +1,36 @@
-﻿// SecondDriverClient.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+﻿// Booster.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+#include "..\SecondDriver\SecondDriverCommon.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int Error(const char* message) {
+	printf("%s (error=%d)\n", message, GetLastError());
+	return 1;
 }
 
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+int main(int argc, const char* argv[]) {
+	if (argc < 3) {
+		printf("Usage: Booster <threadid> <priority>\n");
+		return 0;
+	}
 
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
+	HANDLE hDevice = CreateFile(L"\\\\.\\PriorityBooster", GENERIC_WRITE, FILE_SHARE_WRITE,
+		nullptr, OPEN_EXISTING, 0, nullptr);
+	if (hDevice == INVALID_HANDLE_VALUE)
+		return Error("Failed to open device");
+
+	ThreadData data;
+	data.ThreadId = atoi(argv[1]);
+	data.Priority = atoi(argv[2]);
+
+	DWORD returned;
+	BOOL success = DeviceIoControl(hDevice, IOCTL_SECOND_DRIVER_SET_PRIORITY, &data, sizeof(data), nullptr, 0, &returned, nullptr);
+	if (success)
+		printf("Priority change succeeded!\n");
+	else
+		Error("Priority change failed!");
+
+	CloseHandle(hDevice);
+}
