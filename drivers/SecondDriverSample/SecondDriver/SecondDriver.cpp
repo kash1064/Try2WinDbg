bff0bb348d44354646d05065a7a81c0cf8b0fb0b
kash1064 <kaship24@gmail.com>
2022-01-29 13:24:20 +0900

    fix SecondDriver Solution

--- drivers/SecondDriverSample/SecondDriver/SecondDriver.cpp
+++ drivers/SecondDriverSample/SecondDriver/SecondDriver.cpp
@@ -1,4 +1,6 @@
+#include <ntifs.h>
 #include <ntddk.h>
+#include "SecondDriverCommon.h"
 
 void SecondDriverUnload(_In_ PDRIVER_OBJECT DriverObject);
 NTSTATUS SecondDriverCreateClose(_In_ PDRIVER_OBJECT DriverObject, _In_ PIRP irp);
@@ -45,4 +47,63 @@ DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 	KdPrint(("SecondDriverSecondDriver DriverEntry completed successfully\n"));
 
 	return STATUS_SUCCESS;
+}
+
+_Use_decl_annotations_
+NTSTATUS SecondDriverCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+_Use_decl_annotations_
+NTSTATUS SecondDriverDeviceControl(PDEVICE_OBJECT, PIRP Irp) {
+	// get our IO_STACK_LOCATION
+	auto stack = IoGetCurrentIrpStackLocation(Irp);
+	auto status = STATUS_SUCCESS;
+
+	switch (stack->Parameters.DeviceIoControl.IoControlCode) {
+	case IOCTL_SECOND_DRIVER_SET_PRIORITY:
+	{
+		// do the work
+		if (stack->Parameters.DeviceIoControl.InputBufferLength < sizeof(ThreadData)) {
+			status = STATUS_BUFFER_TOO_SMALL;
+			break;
+		}
+
+		auto data = (ThreadData*)stack->Parameters.DeviceIoControl.Type3InputBuffer;
+		if (data == nullptr) {
+			status = STATUS_INVALID_PARAMETER;
+			break;
+		}
+
+		if (data->Priority < 1 || data->Priority > 31) {
+			status = STATUS_INVALID_PARAMETER;
+			break;
+		}
+
+		PETHREAD Thread;
+		status = PsLookupThreadByThreadId(ULongToHandle(data->ThreadId), &Thread);
+		if (!NT_SUCCESS(status))
+			break;
+
+		KeSetPriorityThread((PKTHREAD)Thread, data->Priority);
+		ObDereferenceObject(Thread);
+		KdPrint(("Thread Priority change for %d to %d succeeded!\n",
+			data->ThreadId, data->Priority));
+		break;
+	}
+
+	default:
+		status = STATUS_INVALID_DEVICE_REQUEST;
+		break;
+	}
+
+	Irp->IoStatus.Status = status;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return status;
 }
---
+++ drivers/SecondDriverSample/SecondDriver/SecondDriverCommon.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct ThreadData {
+	ULONG ThreadId;
+	int Priority;
+};
+
+#define SECOND_DRIVER_DEVICE 0x8000
+#define IOCTL_SECOND_DRIVER_SET_PRIORITY CTL_CODE(SECOND_DRIVER_DEVICE, 0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
