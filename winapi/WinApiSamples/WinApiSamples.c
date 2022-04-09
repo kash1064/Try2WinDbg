be24fbbf2f4f893198a89bfce739d369ac587ba3
kash1064 <kaship24@gmail.com>
2022-04-09 09:12:33 +0900

    add api sample

---
+++ winapi/WinApiSamples/WinApiSamples.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+int main(void) {
+    int lpBuffer[] = { 1 };
+    const WCHAR* lpFileName = L"C:\\Users\\Tadpole01\\Downloads\\test.txt";
+
+    HANDLE hFile = CreateFile(
+        lpFileName,
+        GENERIC_WRITE,
+        FILE_SHARE_READ,
+        NULL,
+        CREATE_ALWAYS,
+        0,
+        NULL
+    );
+
+    DWORD nNumberOfBytesToWrite = sizeof(lpBuffer);
+    DWORD lpNumberOfBytesWritten;
+
+    BOOL bResult = WriteFile(
+        hFile,
+        lpBuffer,
+        nNumberOfBytesToWrite,
+        &lpNumberOfBytesWritten,
+        NULL
+    );
+
+    CloseHandle(hFile);
+	return 0;
+}
